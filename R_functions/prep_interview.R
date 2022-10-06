e53733b1c683fe52e94da113392664928f22d14e
evanbooher <evan.c/booher@gmail.com>
2022-10-06 10:35:56 -0700

    making assignment of interviews with boat_used == "Yes" & fish_from_boat == "Bank" boat angler type (left NA as is)

--- R_functions/prep_interview.R
+++ R_functions/prep_interview.R
@@ -33,8 +33,10 @@ prep_interview <- function(
         #allow kayaks as "boat": boat_used == "Yes" & stringr::str_detect(boat_type, "ontoon|ayak") & fish_from_boat == "Boat" ~ "boat",
         boat_used == "Yes" & 
           !stringr::str_detect(boat_type, "ontoon|ayak") & 
-          (is.na(fish_from_boat) | fish_from_boat == "Boat") ~ "boat"
-        #allow motorboats as "bank": boat_used == "Yes" & !stringr::str_detect(boat_type, "ontoon|ayak") & fish_from_boat == "Bank" ~ "bank"
+          (is.na(fish_from_boat) | fish_from_boat == "Boat") ~ "boat",
+        boat_used == "Yes" & !stringr::str_detect(boat_type, "ontoon|ayak") & 
+          fish_from_boat == "Bank" ~ "boat"
+        # EB these cases were left NA when tested on skagit fall salmon 2022 data; assigning to boat under current angler_type assignment protocol
       ),
       angler_final_int = as.integer(factor(angler_final)),
       
