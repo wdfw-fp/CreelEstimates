d5751ea45ad9f86938bd01b343348cdbc1cae762
daauerbach <daniel.auerbach@dfw.wa.gov>
2022-11-08 14:36:25 -0800

    viable first draft of multiple bss est_cg iteration, preserving overview and daily catch and effort for now

---
+++ R_functions/get_bss_catch_daily.R
@@ -0,0 +1,18 @@
+get_bss_catch_daily <- function(bss_fit, ecg, ...){
+  bss_fit |> 
+    summary(pars = c("C")) |> 
+    pluck("summary") |> #only want the combined-chains version
+    as.data.frame() |> 
+    rownames_to_column("estimate") |> 
+    as_tibble() |> 
+    mutate(indices = str_sub(estimate, 3, 20) |> str_remove("\\]")) |> 
+    separate(
+      col = indices,
+      into = c("section_num", "day_index", "angler_final")
+      ) |> 
+    mutate(
+      across(c(section_num, day_index), as.integer),
+      angler_final = if_else(angler_final == "1", "bank", "boat"),
+      est_cg = ecg
+      )
+}
---
+++ R_functions/get_bss_overview.R
@@ -0,0 +1,16 @@
+get_bss_overview <- function(bss_fit, ecg, ...){
+  bss_fit |> 
+    summary(pars = c("E_sum", "C_sum")) |> 
+    pluck("summary") |> 
+    as.data.frame() |> 
+    rownames_to_column("estimate") |> 
+    as_tibble() |> 
+    bind_cols(
+      bss_fit |> 
+        rstan::get_sampler_params(inc_warmup = FALSE) |> #list of matrices rows-iterations by 6 measures
+        set_names(~paste0("n_div_",1:length(bss_fit@stan_args))) |> 
+        purrr::map_dbl(~.x[, "divergent__"] |> sum()) |> #n-chains cols of n-divergent transitions per chain
+        sum() |> as_tibble_col(column_name = "n_div")
+    ) |> 
+    mutate(est_cg = ecg)
+}
