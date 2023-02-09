e717679bab0dc1642a7ff872b884721aea27459a
evanbooher <evan.c/booher@gmail.com>
2023-02-08 16:30:07 -0800

    Updated plot_est_pe_catch so it can iterate over multiple catch groups for distinct plots. Updated x axis in index effort count plots. Continued work on fw_creel.Rmd

--- R_functions/plot_est_pe_catch.R
+++ R_functions/plot_est_pe_catch.R
@@ -2,10 +2,12 @@
 
 plot_est_pe_catch <- function(
     estimates_pe_catch,
+    est_catch_group,
     ...
 ){
   
-  estimates_pe$catch |> 
+  estimates_pe$catch |>
+    filter(est_cg == est_catch_group) |> 
     ggplot(aes(period, est, fill = angler_final, color = angler_final)) +
     geom_col(position = position_stack()) +
     facet_wrap(~est_cg + section_num, scales = "fixed", labeller = label_wrap_gen(multi_line = F), ncol = 2) 
--- R_functions/plot_inputs_pe_index_effort_counts.R
+++ R_functions/plot_inputs_pe_index_effort_counts.R
@@ -10,7 +10,7 @@ plot_inputs_pe_index_effort_counts <- function(
     ggplot(aes(event_date, count_index, fill = count_sequence, color = count_sequence)) +
     #geom_point() + geom_text(aes(label = count_index), nudge_y = 1, check_overlap = T) +
     geom_col(position = position_dodge(width = 0.7)) +
-    scale_x_date("", date_breaks = "3 days", date_labels =  "%m-%d") + scale_y_continuous("") +
+    scale_x_date("", date_breaks = "7 days", date_labels =  "%m-%d") + scale_y_continuous("") +
     scale_color_brewer(palette = "Set2", aesthetics = c("color", "fill")) +
     facet_wrap(~section_num + count_type, scales = "fixed", ncol = 1, labeller = label_wrap_gen(multi_line = F))
    
