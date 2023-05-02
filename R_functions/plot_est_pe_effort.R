e10b16d755c45bc417da0bd99b39a30a8c812682
evanbooher <evan.c.booher@gmail.com>
2023-05-02 12:44:52 -0700

    Added time_period strata (week or month) to x-axis of PE effort, CPUE, and catch bar plots.

--- R_functions/plot_est_pe_effort.R
+++ R_functions/plot_est_pe_effort.R
@@ -2,26 +2,43 @@
 
 plot_est_pe_effort <- function(
     estimates_pe_effort,
+    period_pe,
     ...
 ){
-
-  # x axis uses period
-  
-  # estimates_pe$effort |> 
-  #   ggplot(aes(period, est, fill = interaction(day_type, angler_final))) +
-  #   labs(fill = "Angler and day type groups") +
-  #   geom_col(position = position_stack(), color = "black") +
-  #   scale_color_brewer(palette = "Blues", aesthetics = c("fill")) +
-  #   facet_wrap(~section_num, scales = "fixed", labeller = label_wrap_gen(multi_line = F), ncol = 2) 
-  
-  # x axis uses min_event_date
-  
+  if(period_pe == "week"){
   estimates_pe$effort |> 
     ggplot(aes(min_event_date, est, fill = interaction(day_type, angler_final))) +
-    scale_x_date() +
+    scale_x_date(date_breaks = "1 week", labels = scales::date_format("%W"),
+                 sec.axis = dup_axis(name = "", breaks = waiver(), labels = scales::date_format("%b"))) +
     labs(fill = "Angler and day type groups") +
+    ylab("Effort (angler hours)") +
+    xlab("Date (week)") +
     geom_col(position = position_stack(), color = "black", width = 3.5) +
     scale_color_brewer(palette = "Blues", aesthetics = c("fill")) +
     facet_wrap(~section_num, scales = "fixed", labeller = label_wrap_gen(multi_line = F), ncol = 2) 
-   
 }
+else if(period_pe == "month"){
+  estimates_pe$effort |> 
+    ggplot(aes(min_event_date, est, fill = interaction(day_type, angler_final))) +
+    scale_x_date(date_breaks = "1 month", labels = scales::date_format("%b")) +
+    labs(fill = "Angler and day type groups") +
+    ylab("Effort (angler hours)") +
+    xlab("Date (month)") +
+    geom_col(position = position_stack(), color = "black", width = 12) +
+    scale_color_brewer(palette = "Blues", aesthetics = c("fill")) +
+    facet_wrap(~section_num, scales = "fixed", labeller = label_wrap_gen(multi_line = F), ncol = 2) 
+  }
+  
+else if(period_pe == "duration"){
+      estimates_pe$effort |> 
+        ggplot(aes(min_event_date, est, fill = interaction(day_type, angler_final))) +
+        scale_x_date() +
+        labs(fill = "Angler and day type groups") +
+        ylab("Effort (angler hours)") +
+        xlab("Date") +
+        geom_col(position = position_stack(), color = "black", width = 12) +
+        scale_color_brewer(palette = "Blues", aesthetics = c("fill")) +
+        facet_wrap(~section_num, scales = "fixed", labeller = label_wrap_gen(multi_line = F), ncol = 2) 
+  }
+}
+
--- R_functions/plot_inputs_pe_cpue_period.R
+++ R_functions/plot_inputs_pe_cpue_period.R
@@ -6,36 +6,10 @@ plot_inputs_pe_cpue_period <- function(
     dwg_summarized, #list with shared interview, index and census tibbles
     daily_cpue_catch_est,
     est_catch_group,
+    period_pe,
     ...
 ){
   
-  # cpue_period <- dplyr::left_join(
-  #   dwg_summarized$interview
-  #   , 
-  #   days |> dplyr::select(event_date, day_type, period)
-  #   ,
-  #   by=c("event_date")
-  # ) |>
-  #   dplyr::filter(est_cg == est_catch_group) |> 
-  #   dplyr::group_by(section_num, period, angler_final, est_cg) |> #day_type dropped to simplify plot 
-  #   dplyr::summarise(
-  #     n_obs = n(),
-  #     total_catch = sum(fish_count),
-  #     total_hours = sum(fishing_time_total),
-  #     cpue_rom_period = total_catch / total_hours,
-  #     .groups = "drop"
-  #   )
-  # 
-  # cpue_period |> 
-  #   ggplot(aes(period, cpue_rom_period, fill = angler_final, color = angler_final)) +
-  #   geom_point(size = 3) +
-  #   # scale_x_date("", date_breaks = "7 days", date_labels =  "%m-%d") + scale_y_continuous("") + # if using dates for x-axis
-  #   scale_color_brewer(palette = "Set2", aesthetics = c("color", "fill")) +
-  #   labs(title = est_catch_group) +
-  #   # geom_text(aes(label = n_obs), nudge_y = 0.02, color = "black", check_overlap = TRUE, size = 2.5) + #option to see sample size
-  #   facet_wrap(~section_num, scales = "fixed", ncol = 2, labeller = label_wrap_gen(multi_line = F))
-
-
   cpue_period <- dplyr::left_join(
     dwg_summarized$interview
     , 
@@ -62,29 +36,44 @@ plot_inputs_pe_cpue_period <- function(
       by = "period"
     )
   
-  # x axis uses period
+  if(period_pe == "week"){
+    cpue_period |> 
+      ggplot(aes(min_event_date, cpue_rom_period, fill = interaction(day_type, angler_final))) +
+      geom_point(aes(fill = interaction(day_type, angler_final)), color = "black", pch = 21, size = 3.25) +
+      scale_x_date(date_breaks = "1 week", labels = scales::date_format("%W"),
+                   sec.axis = dup_axis(name = "", breaks = waiver(), labels = scales::date_format("%b"))) +
+      ylab("Catch per unit effort (fish/hr)") +
+      xlab("Date (week)") +
+      labs(title = est_catch_group, fill = "Angler and day type groups") +
+      scale_color_brewer(palette = "Blues", aesthetics = c("fill")) +
+      # geom_text(aes(label = n_obs), nudge_y = 0.02, color = "black", check_overlap = TRUE, size = 2.5) + #option to see sample size
+      facet_wrap(~section_num, scales = "fixed", ncol = 2, labeller = label_wrap_gen(multi_line = F))
+  }
+  else if(period_pe == "month"){
+    
+    cpue_period |> 
+      ggplot(aes(min_event_date, cpue_rom_period, fill = interaction(day_type, angler_final))) +
+      geom_point(aes(fill = interaction(day_type, angler_final)), color = "black", pch = 21, size = 3.25) +
+      scale_x_date(date_breaks = "1 month", labels = scales::date_format("%b")) +
+      ylab("Catch per unit effort (fish/hr)") +
+      xlab("Date (month)") +
+      labs(title = est_catch_group, fill = "Angler and day type groups") +
+      scale_color_brewer(palette = "Blues", aesthetics = c("fill")) +
+      # geom_text(aes(label = n_obs), nudge_y = 0.02, color = "black", check_overlap = TRUE, size = 2.5) + #option to see sample size
+      facet_wrap(~section_num, scales = "fixed", ncol = 2, labeller = label_wrap_gen(multi_line = F))
+    
+  }
   
-  # cpue_period |> 
-  #   ggplot(aes(period, cpue_rom_period, fill = interaction(day_type, angler_final))) +
-  #   # geom_jitter(aes(color = interaction(day_type, angler_final)), size = 3) +
-  #   geom_point(aes(fill = interaction(day_type, angler_final)), color = "black", pch = 21, size = 3.25) +
-  #   labs(title = est_catch_group, fill = "Angler and day type groups") +
-  #   # scale_x_date("", date_breaks = "7 days", date_labels =  "%m-%d") + scale_y_continuous("") + # if using dates for x-axis
-  #   scale_color_brewer(palette = "Blues", aesthetics = c("fill")) +
-  #   # geom_text(aes(label = n_obs), nudge_y = 0.02, color = "black", check_overlap = TRUE, size = 2.5) + #option to see sample size
-  #   facet_wrap(~section_num, scales = "fixed", ncol = 2, labeller = label_wrap_gen(multi_line = F))
-  
-  
-  # x axis uses min_event_date
-  
-  cpue_period |> 
-    ggplot(aes(min_event_date, cpue_rom_period, fill = interaction(day_type, angler_final))) +
-    # geom_jitter(aes(color = interaction(day_type, angler_final)), size = 3) +
-    geom_point(aes(fill = interaction(day_type, angler_final)), color = "black", pch = 21, size = 3.25) +
-    labs(title = est_catch_group, fill = "Angler and day type groups") +
-    # scale_x_date("", date_breaks = "7 days", date_labels =  "%m-%d") + scale_y_continuous("") + # if using dates for x-axis
-    scale_color_brewer(palette = "Blues", aesthetics = c("fill")) +
-    # geom_text(aes(label = n_obs), nudge_y = 0.02, color = "black", check_overlap = TRUE, size = 2.5) + #option to see sample size
-    facet_wrap(~section_num, scales = "fixed", ncol = 2, labeller = label_wrap_gen(multi_line = F))
-  
+  else if(period_pe == "duration"){
+    cpue_period |> 
+      ggplot(aes(min_event_date, cpue_rom_period, fill = interaction(day_type, angler_final))) +
+      geom_point(aes(fill = interaction(day_type, angler_final)), color = "black", pch = 21, size = 3.25) +
+      scale_x_date() +
+      ylab("Catch per unit effort (fish/hr)") +
+      xlab("Date") +
+      labs(title = est_catch_group, fill = "Angler and day type groups") +
+      scale_color_brewer(palette = "Blues", aesthetics = c("fill")) +
+      # geom_text(aes(label = n_obs), nudge_y = 0.02, color = "black", check_overlap = TRUE, size = 2.5) + #option to see sample size
+      facet_wrap(~section_num, scales = "fixed", ncol = 2, labeller = label_wrap_gen(multi_line = F))
+  }
 }
