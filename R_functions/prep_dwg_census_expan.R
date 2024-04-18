9ea3567605d71e410530a68ef3b53a3058c84b3f
Bentley <Kale.Bentley@dfw.wa.gov>
2024-04-18 16:28:32 -0700

    moved notes from .Rmd to function

--- R_functions/prep_dwg_census_expan.R
+++ R_functions/prep_dwg_census_expan.R
@@ -1,3 +1,9 @@
+# creates summary table of census expansion factors based on values in data base
+
+#NOTE: "p_census is the same as "p_TI" in the BSS model, which represent a known or assumed proportion of each section covered by tie-in counts for each final angler-type; this parameter is used to directly expand/modify the census count data prior to the estimate of the bias parameter/expansion factor
+#NOTE: values of "p_census/p_TI" are entered and stored in creel db
+#NOTE: currently, values of "p_census/p_TI" are limited to two "angler_final" groupings (bank, boat)
+
 prep_dwg_census_expan <- function(
     eff, # effort data from dwg filtered using start & end dates passed from params
     ...){
