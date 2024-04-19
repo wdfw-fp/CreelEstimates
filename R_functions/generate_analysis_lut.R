5859685d98b2b3b4ecb6db614cc487e69e013a9c
Holley <Colt.Holley@dfw.wa.gov>
2024-04-19 13:40:14 -0700

    r_session_json info now performed by json_conversion()

--- R_functions/generate_analysis_lut.R
+++ R_functions/generate_analysis_lut.R
@@ -1,23 +1,9 @@
-#Function to generate an analysis look up table with session-specific uuid and session info
+#Function to generate base analysis look up table with session-specific uuid
 generate_analysis_lut <- function(params) {
   #check that analysis uuid
   if (!exists("analysis_lut", envir = .GlobalEnv)) {
- 
-    #initialize look up table that records metadata for each analysis session
-    analysis_lut <<- data.frame(analysis_id = character(0), 
-                                analysis_name = character(0),
-                                repo_version = character(0),
-                                r_session_json = character(0)
-                                ) #fishery_regulations_json and analysis_json fields added by other processes later in ETL
-
-    # record R session info and convert to JSON
-    user <- paste("User:", Sys.info()["user"])
-    analysis_session_info <- sessioninfo::session_info() |> capture.output()
-    analysis_session_info[1] <- paste(user, analysis_session_info[1])
-    analysis_session_info <- analysis_session_info |> 
-      jsonlite::toJSON(pretty = TRUE)
     
-    #Generate uuid and put in analysis_lut
+    #generate session-specific analysis uuid
     analysis_id <- uuid::UUIDgenerate()
     
     analysis_lut <<- rbind(analysis_lut, 
@@ -27,7 +13,6 @@ generate_analysis_lut <- function(params) {
                                                       #ISO 8601 datetime format
                                                       format(Sys.time(), format = "%Y-%m-%dT%H:%M:%SZ")
                                                     ),
-                                      r_session_json = analysis_session_info,
                                       repo_version = paste0("https://github.com/wdfw-fp/CreelEstimates/tree/",
                                                             system("git rev-parse HEAD", intern = TRUE))
                                       )
