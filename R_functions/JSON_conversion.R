ba3d328f3f39414dd26a70652c7d90eb8300cfea
Holley <Colt.Holley@dfw.wa.gov>
2024-06-21 14:01:41 -0700

    Adding messages to inform user of conversion status/error handling.

--- R_functions/JSON_conversion.R
+++ R_functions/JSON_conversion.R
@@ -5,7 +5,6 @@ json_conversion <- function(type, ...) {
 #check type
   valid_types <- c("script", "regulations", "r_session")
   if (!type %in% valid_types) {
-    
     stop("Invalid type argument. Select 'script', 'regulations', or 'r_session'")
   }
   
@@ -15,6 +14,8 @@ json_conversion <- function(type, ...) {
     #save local script to archive any alterations from template script
     rstudioapi::documentSave()
     
+    cat("\nLocal analysis script saved.")
+    
     #locate script location on CreelEstimates local repository clone
     analysis_script <- readLines(paste0(
       getwd(), "/fishery_analyses/", params$project_name, "/", params$fishery_name,
@@ -23,30 +24,40 @@ json_conversion <- function(type, ...) {
     
     #convert to JSON format and validate
     json_script <- jsonlite::toJSON(analysis_script, pretty = TRUE)
+    
+    cat("\nLocal script read and converted to JSON format.")
+    
     valid <- jsonlite::validate(json_script)
     
     if(valid) { #if TRUE
       
+      cat("\nJSON valid... adding `analysis_json` to analysis_lut.")
+      
       #add to analysis look up table 
       analysis_lut <<- analysis_lut |>  dplyr::mutate(analysis_json = json_script)
+      
+    } else {
+      warning("\nJSON format not valid! JSON representation of local script not added to analysis_lut.")
     }
   }
   if (type == "regulations") {
     
-    #requires an established ODBC connection to the FISH database named "con"
-    if (!DBI::dbIsValid(con)) {
-      stop("Must be connected to the FISH database via ODBC connection named 'con'.")
-    }
-    
-    #query database for regulations
-    
-    #convert to json and validate
-    
-    if (valid) {
-      #add to analysis look up table      
-    }
+    # #requires an established ODBC connection to the FISH database named "con"
+    # if (!DBI::dbIsValid(con)) {
+    #   stop("Must be connected to the FISH database via ODBC connection named 'con'.")
+    # }
+    # 
+    # #query database for regulations
+    # 
+    # #convert to json and validate
+    # 
+    # if (valid) {
+    #   #add to analysis look up table      
+    # }
   } 
   if (type == "r_session") {
+    
+    cat("\nGathering User and R session information.")
     
     #record user and R session info
     user <- paste("User:", Sys.info()["user"])
@@ -55,11 +66,20 @@ json_conversion <- function(type, ...) {
     
     #convert to json and validate
     r_session_json <- r_session |> jsonlite::toJSON(pretty = TRUE)
+    
+    cat("\nUser and R session information converted to JSON format.")
+    
     valid <- jsonlite::validate(r_session_json)
     
     if (valid) {
+      
+      cat("\nJSON valid... adding `r_session_json` to analysis_lut.")
+      
       #add to analysis look up table
       analysis_lut <<- analysis_lut |>  dplyr::mutate(r_session_json = r_session_json)
+      
+    } else {
+      warning("\nJSON format not valid! JSON representation of R session information not added to analysis_lut.")
     }
   } 
 }
@@ -67,53 +87,3 @@ json_conversion <- function(type, ...) {
 #examples
 # json_conversion(type = "script")
 # json_conversion(type = "r_session")
-
-
-
-# ORIGINAL VERSION OF FUNCTION, 4/18/24 C.H.
-# ### ----------------------------------------------------------------------- ####
-# 
-# JSON_conversion <- function(params, direction) {
-#   
-#   if (direction == "toJSON") {
-#     #save local script to archive any alterations from template script
-#     rstudioapi::documentSave()
-#     
-#     #locate script location on local repository clone
-#     analysis_script <- readLines(paste0(
-#       getwd(), "/fishery_analyses/", params$project_name, "/", params$fishery_name,
-#       "/", "fw_creel_", params$fishery_name, ".Rmd"
-#       ))
-#     
-#     #convert to JSON format and validate
-#     JSON_script <- jsonlite::toJSON(analysis_script, pretty = TRUE)
-#     valid <- jsonlite::validate(JSON_script)
-#     
-#     if(valid) { #if TRUE
-#       
-#       #add JSON string into creel analysis look up table as new column
-#       analysis_lut <<- analysis_lut %>% 
-#         mutate(analysis_json = JSON_script)
-#       
-#     } else {
-#         stop("Not a valid JSON string")
-#     }
-#   
-#   } else if (direction == "fromJSON") {
-#     
-#     #convert back from JSON format
-#     parse_JSON <- jsonlite::fromJSON(analysis_lut$analysis_json)
-#     reconstructed_script <- paste(parse_JSON, collapse = "\n")
-#     
-#     #export reconstructed analysis file
-#     writeLines(reconstructed_script, paste0(params$project_name, "_", params$fishery_name,
-#                                             "_", "reconstructed_analysis.Rmd"))
-#     
-#     #open file for viewing
-#     file.edit(paste0(getwd(),"/", params$project_name, "_", params$fishery_name,
-#                      "_", "reconstructed_analysis.Rmd"))
-#     
-#   } else {
-#     stop("Invalid direction argument. Choose 'toJSON' or 'fromJSON'.")
-#   }
-# }
