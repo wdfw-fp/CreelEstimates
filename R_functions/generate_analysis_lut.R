8dd08b224ab8607380ef65bbcf35791ed3187dcd
Holley <Colt.Holley@dfw.wa.gov>
2024-06-21 15:18:31 -0700

    Dropping <<- operator in favor of return()

--- R_functions/generate_analysis_lut.R
+++ R_functions/generate_analysis_lut.R
@@ -8,8 +8,9 @@ generate_analysis_lut <- function(params) {
     
     #generate session-specific analysis uuid
     analysis_id <- uuid::UUIDgenerate()
+    cat("\nUnique analysis_id created for this R session.")
     
-    analysis_lut <<- rbind(analysis_lut, 
+    analysis_lut <- rbind(analysis_lut, 
                            data.frame(analysis_id,
                                       analysis_name = paste0(
                                                       params$fishery_name, "_", params$data_grade, "_",
@@ -20,8 +21,9 @@ generate_analysis_lut <- function(params) {
                                                             system("git rev-parse HEAD", intern = TRUE))
                                       )
                            )
-    cat("Unique analysis_id created for this R session.")
+    return(analysis_lut)
+    
     } else {
-    cat("analysis_lut with unique 'analysis_id' already generated in this R session.")
+    cat("\nanalysis_lut with unique 'analysis_id' already generated in this R session.")
   }
 }
