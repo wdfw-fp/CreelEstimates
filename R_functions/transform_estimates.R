39084624fa868b0dad579531225ad6ed51318f06
Holley <Colt.Holley@dfw.wa.gov>
2024-06-26 16:44:56 -0700

    establish transform_estimates as a separation from export_estimates to shorten

---
+++ R_functions/transform_estimates.R
@@ -0,0 +1,64 @@
+transform_estimates <- function(dwg, transformed_pe_data, transformed_bss_data) {
+  #Combine PE and BSS standardized objects
+  
+  creel_estimates <- list(
+    #table 1
+    stratum_catch = rbind(transformed_pe_data$pe_stratum_catch, transformed_bss_data$bss_stratum_catch),
+    #table 2
+    stratum_effort = rbind(transformed_pe_data$pe_stratum_effort, transformed_bss_data$bss_stratum_effort),
+    #table 3
+    summarized_catch = rbind(transformed_pe_data$pe_summarized_catch, transformed_bss_data$bss_summarized_catch),
+    #table 4
+    summarized_effort = rbind(transformed_pe_data$pe_summarized_effort, transformed_bss_data$bss_summarized_effort)
+  )
+  
+  #combine catch and effort data
+  creel_estimates$stratum <- rbind(creel_estimates$stratum_catch, creel_estimates$stratum_effort)
+  creel_estimates$total <- rbind(creel_estimates$summarized_catch, creel_estimates$summarized_effort)
+  
+  #bring in fishery manager table
+  dwg$fishery_manager$catch_area_code <- as.character(dwg$fishery_manager$catch_area_code)
+  
+  fishery_manager_trim <- dwg$fishery_manager |>
+    filter(location_type == "Section") |> 
+    select(section_num, catch_area_code, catch_area_description) |>
+    arrange(section_num) |> 
+    distinct() #assumes no duplicate section_num in fishery manager table
+  
+  #join CRC from fishery manager table to estimate tables
+  creel_estimates$stratum <- left_join(creel_estimates$stratum, fishery_manager_trim, by ="section_num")
+  
+  # fishery_manager_total <- as.vector(fishery_manager_trim$catch_area_code)
+  # fishery_manager_total <- paste(fishery_manager_total, collapse = ",")
+  # #join CRCs from all sections for total strata
+  # creel_estimates$total <- creel_estimates$total |> mutate(catch_area_code = fishery_manager_total)
+  
+  #rename value column to estimate_value
+  creel_estimates$stratum <- creel_estimates$stratum |> rename(estimate_value = value)
+  creel_estimates$total <- creel_estimates$total |> rename(estimate_value = value)  
+  
+  # change angler_final capitalization to match creel database lut
+  creel_estimates$stratum <- creel_estimates$stratum |> 
+    mutate(angler_final = case_when(
+      angler_final == "bank" ~ "Bank",
+      angler_final == "boat" ~ "Boat"
+    ))
+  
+  # add period_timestep field to denote yaml model parameters
+  creel_estimates$stratum <- creel_estimates$stratum |> 
+    mutate(period_timestep = case_when(
+      model_type == "PE" ~ params$period_pe,
+      model_type == "BSS" ~ params$period_bss
+    ))
+  
+  creel_estimates$total <- creel_estimates$total |> 
+    mutate(period_timestep = case_when(
+      model_type == "PE" ~ params$period_pe,
+      model_type == "BSS" ~ params$period_bss
+    ))
+  
+  cat("\nTransformed output object 'creel_estimates' created.")
+  
+  return(creel_estimates)
+
+}
