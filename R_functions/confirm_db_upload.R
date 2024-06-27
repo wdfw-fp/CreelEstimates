c4abe56b334b54c24d4522465d659f2b0efdf911
Holley <Colt.Holley@dfw.wa.gov>
2024-06-27 12:23:50 -0700

    correct verification check and reformat messages

--- R_functions/confirm_db_upload.R
+++ R_functions/confirm_db_upload.R
@@ -2,23 +2,21 @@
 #rows, columns, and values have been written. Currently this is in draft form and needs to be more comprehensive.
 
 confirm_db_upload <- function(con, analysis_lut) {
-  
-  #query database for records that match the estimates that were just written
-  fetch_db_table(con, "creel", "model_analysis_lut") |> select(analysis_id, analysis_name)
 
-  if (analysis_lut$analysis_id %in% confirm_db_upload$analysis_id) {
+  #query database for records that match the estimates that were just written
+  verification_table <- fetch_db_table(con, "creel", "model_analysis_lut") |> select(analysis_id, analysis_name)
+
+  if (analysis_lut$analysis_id %in% verification_table$analysis_id) {
     
     DBI::dbDisconnect(con)
-    cat(paste("Data sucessfully exported.", "\u2713","\n"))
-    cat("Disconnecting from database.\n")
+    cat(paste("\nData sucessfully exported.", "\u2713"))
+    cat("\nDisconnecting from database.")
     
   } else {
     #what to do if analysis_id is not in analysis_lut (partial/failed export)
-    cat("\n")
-    message("Unable to confirm upload by checking database for session analysis_id.")
+    message("\nUnable to confirm upload by checking database for session analysis_id.")
     
-    cat("\n")
-    message(paste("writing",crayon::red$bgYellow("FAILED_UPLOAD_LOG_analysis_lut.csv") , "to CreelEstimates folder so that analysis_id for partial data upload can be investigated."))
+    message(paste("\nWriting",crayon::red$bgYellow("FAILED_UPLOAD_LOG_analysis_lut.csv") , "to CreelEstimates folder so that analysis_id for partial data upload can be investigated."))
     
     readr::write_csv(analysis_lut, file = paste0("FAILED_UPLOAD_LOG_","analysis_lut.csv"), append = TRUE)
     
