ff6e317c165ae906c0e49668d43efd0f6277d38b
Holley <Colt.Holley@dfw.wa.gov>
2024-10-09 17:31:59 -0700

    standardizing naming and applying snake_case

--- R_functions/transform_estimates.R
+++ R_functions/transform_estimates.R
@@ -56,6 +56,49 @@ transform_estimates <- function(dwg, transformed_pe_data, transformed_bss_data) 
       model_type == "PE" ~ params$period_pe,
       model_type == "BSS" ~ params$period_bss
     ))
+
+  ########################################################################################
+  ### Performing standardization procedures. This cleanup is needed to better align
+  ### different model outputs and in preparation for the development of a data dictionary.
+  ### These edits are a post-hoc approach and we plan to implement these changes earlier
+  ### in the pipeline during future development phases. - CH 10/9/2024
+  ########################################################################################
+
+  # purrr::map() #across stratum and total tables in creel_estimates
+  
+  x |> 
+    dplyr::mutate(
+      dplyr::case_when( #Make BSS outputs match PE
+        estimate_category = "C_daily" ~ "catch",
+        estimate_category = "E_daily" ~ "effort",
+        estimate_category = "CPUE_daily" ~ "catch_per_unit_effort"
+      ),
+      dplyr::case_when(
+        #apply snake_case to estimate_type values
+        estimate_type = "totalobs" ~ "total_observations", ### consider moving to analysis_lut
+        estimate_type = "N_days_total" ~ "n_days_open", ### consider moving to analysis_lut
+        estimate_type = "totaldaysopen" ~ "total_days_open", ### consider moving to analysis_lut
+        
+        estimate_type = "Rhat" ~ "r_hat",
+        estimate_type = "n_div" ~ "number_of_divisions",
+        estimate_type = "n_eff" ~ "number_of_draws", #https://mc-stan.org/docs/cmdstan-guide/stansummary.html
+        estimate_type = "df" ~ "degrees_of_freedom",
+        estimate_type = "sd" ~ "standard_deviation",
+        estimate_type = "se_mean", "standard_error_of_mean",
+        estimate_type = "est" ~"estimate" #applies to catch & effort, which are identified by model_type field,
+        estimate_type = "catch_est_mean" ~ "catch_estimate_mean",
+        estimate_type = "catch_est_var" ~ "catch_estimate_variance",,
+        estimate_type = "ang_hrs_mean" ~ "angler_hours_mean", #or mean_angler_hours ?
+        estimate_type = "ang_hrs_var" ~ "angler_hours_variance",
+        
+        estimate_type = "2.5_pct" ~ "lower_quantile_2_5", #maybe quantile_lower_2_5 or quantile_2_5_percent
+        estimate_type = "25_pct" ~ "lower_quantile_25",
+        estimate_type = "50_pct" ~ "median_quantile_50",
+        estimate_type = "75_pct" ~ "upper_quantile_75",
+        estimate_type = "97.5_pct" ~ "upper_quantile_97_5",
+        TRUE ~ estimate_type
+      )
+    )
   
   cat("\nTransformed output object 'creel_estimates' created.")
   
